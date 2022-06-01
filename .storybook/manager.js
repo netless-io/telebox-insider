e4bee05feafbb8ef9259c1d5043623131d043fc9
CRIMX <straybugs@gmail.com>
2022-06-01 11:17:04 +0800

    chore: remove storybook

--- .storybook/manager.js
+++
@@ -1,6 +0,0 @@
-import { addons } from "@storybook/addons";
-import teleBoxTheme from "./telebox-theme";
-
-addons.setConfig({
-    theme: teleBoxTheme,
-});
--- .storybook/telebox-theme.js
+++
@@ -1,8 +0,0 @@
-import { create } from "@storybook/theming";
-
-export default create({
-    base: "light",
-    brandTitle: "TeleBox",
-    brandUrl: "https://github.com/netless-io/telebox",
-    // brandImage: brandImage,
-});
